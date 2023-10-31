11659ca6115c20f6d0285313380e88417577fe79
Wahyu Budi Saputra <wahyubucil@gmail.com>
2023-10-31 20:04:49 +0800

    build: config package

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+const { configure, presets } = require('eslint-kit')
+
+module.exports = configure({
+  allowDebug: process.env.NODE_ENV !== 'production',
+  presets: [
+    presets.imports(),
+    presets.node(),
+    presets.prettier(),
+    presets.typescript(),
+  ],
+})
