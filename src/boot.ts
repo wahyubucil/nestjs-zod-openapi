8467a4b2712d81de0ac59d013e8bfc3f0bc1dc86
Wahyu Budi Saputra <wahyubucil@gmail.com>
2023-11-01 11:30:01 +0800

    fix: export global override of zod types

--- src/boot.ts
+++ src/boot.ts
@@ -2,3 +2,6 @@ import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi'
 import { z } from 'zod'
 
 extendZodWithOpenApi(z)
+
+// To export the global override of Zod types
+export { ZodOpenAPIMetadata } from '@asteasolutions/zod-to-openapi'
